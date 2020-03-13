f5b85cfaa74115d2ca427554bac9ec484e4cff10
Bilbertius <lauchlinmacneil@gmail.com>
2020-03-13 15:04:03 -0600

    Link deployments

--- modules/app/app.js
+++ modules/app/app.js
@@ -3,9 +3,9 @@ const cors = require('cors');
 const config = require('../../config');
 const app = express();
 
-app.use(cors({
-	origin: config.CLIENT_ORIGIN
-}));
+app.use(cors());
+
+app.options('*', cors());
 
 app.use('/api/people', require('../people/people.router'));
 app.use('/api/pets', require('../pets/pets.router'));
--- modules/pets/pets.router.js
+++ modules/pets/pets.router.js
@@ -6,20 +6,14 @@ const People = require('../people/people.service');
 
 const PetsRouter = express.Router();
 
-PetsRouter
-	.route('/')
-	.get((req, res) => {
+PetsRouter.get('/',(req, res) => {
         const pets = PetsService.get();
 		res.json(pets);
-	
-
-	})
-	.route('/dog')
-	.delete(json, (req, res) => {
+	});
+	PetsRouter.delete('/dog', json, (req, res) => {
 		res.status(200).send(PetsService.dequeue('dog'));
-	})
-	.route('/cat')
-	.delete(json, (req, res) => {
+	});
+	PetsRouter.delete('/cat',json, (req, res) => {
 		res.status(200).send(PetsService.dequeue('cat'));
-	})
+	});
 module.exports = PetsRouter;
