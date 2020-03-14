ca1967370951be558e01004685864eb38c07a032
Bilbertius <lauchlinmacneil@gmail.com>
2020-03-14 01:02:03 -0600

    FEweg

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'https://petful-green.now.sh',
+	CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'https://petfulerest.now.sh',
 	PORT : process.env.PORT || 8080
 };
