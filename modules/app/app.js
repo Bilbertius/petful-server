1980fd4a77fedad3e2ca46aaf2f08182b69eece1
Bilbertius <lauchlinmacneil@gmail.com>
2020-03-14 01:17:16 -0600

    sefpon

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const {CLIENT_ORIGIN} = require('../../config')
 
-app.use(cors({credentials: true, origin: true}));
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", `${CLIENT_ORIGIN}/*`); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 
 app.use('/api/people', require('../people/people.router'));
 app.use('/api/pets', require('../pets/pets.router'));
